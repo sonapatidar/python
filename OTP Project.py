dd0f1df928d16adb3ac19501a17505d740d60371
sonali patidar <88070562+sonapatidar@users.noreply.github.com>
2021-07-30 12:04:00 +0530

    Day 2 Project

---
+++ OTP Project.py
@@ -0,0 +1,11 @@
+import random as r
+import string
+length=8
+otp=''
+characters= string.ascii_letters + string.digits
+print(characters)
+
+for i in range(length):
+    otp=otp + r.choice(characters)
+   
+print("OTP", otp)    
